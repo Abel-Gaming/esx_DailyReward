d9ca3ac269d40dde91738a0ecde5afafa534fd9f
Abel Gaming <lcpdpolice4939@gmail.com>
2022-06-16 05:37:59 -0400

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -9,21 +9,41 @@ if Config.UseCommand then
 	end, false)
 end
 
+RegisterCommand('viewRedeemed', function(source, args)
+    for k,v in pairs(redeemedIdents) do
+        print(v)
+    end
+end, false)
+
 function ClaimReward(source)
     local xPlayer = ESX.GetPlayerFromId(source)
+    local xPlayerName = xPlayer.getName()
     local xPlayerIdentifier = xPlayer.getIdentifier()
-    
+    if Config.EnableDebug then
+        print(xPlayerName .. ' (' .. xPlayerIdentifier .. ') is attempting to redeem their daily reward')
+    end
 
     -- Check to see if the player's identifier exist in the table
     if #redeemedIdents == 0 then
+        -- Debug Message
+        if Config.EnableDebug then
+            print('Redeemed indentifiers is empty')
+        end
+
         -- Add the player to the reward list
         table.insert(redeemedIdents, xPlayerIdentifier)
+        if Config.EnableDebug then
+            print('Added ' .. xPlayerIdentifier .. ' to redeemed identifiers.')
+        end
 
         --- MONEY REWARD ---
         if Config.MoneyReward then
             local crateAmount = Config.MoneyRewards[math.random(#Config.MoneyRewards)]
             xPlayer.addAccountMoney('bank', crateAmount)
             xPlayer.showNotification('$' .. crateAmount .. ' has been added to your bank!')
+            if Config.EnableDebug then
+                print('Gave ' .. xPlayerIdentifier .. ' their daily reward')
+            end
         end
 
         --- ITEM REWARD ---
@@ -31,29 +51,51 @@ function ClaimReward(source)
             xPlayer.addInventoryItem(Config.Item, Config.ItemAmount)
             local itemLabel = ESX.GetItemLabel(Config.Item)
             xPlayer.showNotification('' .. Config.ItemAmount .. ' ' .. itemLabel .. ' has been added to your inventory!')
+            if Config.EnableDebug then
+                print('Gave ' .. xPlayerIdentifier .. ' their daily reward')
+            end
         end
     else
-        for k, v in ipairs(redeemedIdents) do
-            if xPlayerIdentifier == v then
-                xPlayer.showNotification('[ERROR] You have already redeemed your daily reward!')
-            else
-                -- Add the player to the reward list
-                table.insert(redeemedIdents, xPlayerIdentifier)
+        if has_redeemed(redeemedIdents, xPlayerIdentifier) then
+            xPlayer.showNotification('[ERROR] You have already redeemed your daily reward!')
+            if Config.EnableDebug then
+                print('Player: ' .. xPlayerIdentifier .. ' tried to redeem their daily reward again')
+            end
+        else
+            -- Add the player to the reward list
+            table.insert(redeemedIdents, xPlayerIdentifier)
+            if Config.EnableDebug then
+                print('Added ' .. xPlayerIdentifier .. ' to redeemed identifiers.')
+            end
 
-                --- MONEY REWARD ---
-                if Config.MoneyReward then
-                    local crateAmount = Config.MoneyRewards[math.random(#Config.MoneyRewards)]
-                    xPlayer.addAccountMoney('bank', crateAmount)
-                    xPlayer.showNotification('$' .. crateAmount .. ' has been added to your bank!')
+            --- MONEY REWARD ---
+            if Config.MoneyReward then
+                local crateAmount = Config.MoneyRewards[math.random(#Config.MoneyRewards)]
+                xPlayer.addAccountMoney('bank', crateAmount)
+                xPlayer.showNotification('$' .. crateAmount .. ' has been added to your bank!')
+                if Config.EnableDebug then
+                    print('Gave ' .. xPlayerIdentifier .. ' their daily reward')
                 end
-    
-                --- ITEM REWARD ---
-                if Config.ItemReward then
-                    xPlayer.addInventoryItem(Config.Item, Config.ItemAmount)
-                    local itemLabel = ESX.GetItemLabel(Config.Item)
-                    xPlayer.showNotification('' .. Config.ItemAmount .. ' ' .. itemLabel .. ' has been added to your inventory!')
+            end
+
+            --- ITEM REWARD ---
+            if Config.ItemReward then
+                xPlayer.addInventoryItem(Config.Item, Config.ItemAmount)
+                local itemLabel = ESX.GetItemLabel(Config.Item)
+                xPlayer.showNotification('' .. Config.ItemAmount .. ' ' .. itemLabel .. ' has been added to your inventory!')
+                if Config.EnableDebug then
+                    print('Gave ' .. xPlayerIdentifier .. ' their daily reward')
                 end
             end
         end
     end
+end
+
+function has_redeemed(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
 end
